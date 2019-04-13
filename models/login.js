7cdcd3c119d152a715187389586bd1c48898c39e
Chang <ainurlindale@gmail.com>
2019-04-13 21:27:44 +0800

    created login form

---
+++ models/login.js
@@ -0,0 +1,42 @@
+/**
+ * ===========================================
+ * Export model functions as a module
+ * ===========================================
+ */
+module.exports = (dbPoolInstance) => {
+
+  // `dbPoolInstance` is accessible within this function scope
+  const userName = require('../controllers/login.js');
+  console.log(userName.username);
+  let getAll = (data,callback) => {
+
+    console.log(data.username);
+    console.log(callback);
+    let query = 'SELECT * FROM users';
+    console.log(query);
+
+    dbPoolInstance.query(query, (error, queryResult) => {
+      if( error ){
+
+        // invoke callback function with results after query has executed
+        callback(error, null);
+
+      }else{
+
+        // invoke callback function with results after query has executed
+
+        if( queryResult.rows.length > 0 ){
+          callback(null, queryResult.rows);
+
+        }else{
+          callback(null, null);
+
+        }
+      }
+    });
+  };
+
+  return {
+    getAll,
+  };
+};

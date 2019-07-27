b8cf5255964ead8ce56f5f3a976922f5f344eb9e
emijay <hilmi.johari@yahoo.com.sg>
2019-07-27 23:30:25 +0800

    added code to check for user login

--- models/pokemon.js
+++ models/pokemon.js
@@ -76,9 +76,33 @@ module.exports = (dbPoolInstance) => {
     });
   };
 
+  let checkUser = (dataObj, callback) => {
+
+    let query = "SELECT * FROM users WHERE username = $1";
+
+    const values = [dataObj.username];
+
+    dbPoolInstance.query(query, values, (error, queryResult) => {
+      if( error ){
+        // invoke callback function with results after query has executed
+        callback(error, null);
+
+      }else{
+        // invoke callback function with results after query has executed
+        if( queryResult.rows.length > 0 ){
+          callback(null, queryResult.rows);
+        }else{
+          callback(null, null);
+
+        }
+      }
+    });
+  };
+
   return {
     // getAll,
     getAllTweets,
-    createUser
+    createUser,
+    checkUser
   };
 };

a2ab0a68b569df640a0e8041773694a74f641108
emijay <hilmi.johari@yahoo.com.sg>
2019-07-27 22:26:42 +0800

    minor changes to seed file

--- seed.sql
+++ seed.sql
@@ -1,7 +1,5 @@
-INSERT INTO users(username, email) VALUES ('WilliamSmith', 'bill@example.com');
-INSERT INTO users (username, email) VALUES ('BobJones', 'bob@example.com');
 
-INSERT INTO tweets (user_id, tweets) VALUES ( 1 , 'Hello world!');
-INSERT INTO tweets (user_id, tweets) VALUES ( 1 , 'I cannot wait for class to end!');
-INSERT INTO tweets (user_id, tweets) VALUES ( 2 , 'Fine day for fishing!');
-INSERT INTO tweets (user_id, tweets) VALUES ( 2 , 'OMG I am so hungry!');
+INSERT INTO tweets (tweets, user_id) VALUES ( 'Hello world!', 1 );
+INSERT INTO tweets (tweets, user_id) VALUES ( 'I cannot wait for class to end!', 1 );
+INSERT INTO tweets (tweets, user_id) VALUES ( 'Fine day for fishing!', 2 );
+INSERT INTO tweets (tweets, user_id) VALUES ( 'OMG I am so hungry!', 2 );

cb3169a0ce73452ed4b54bc77c1ceb97f73e9281
emijay <hilmi.johari@yahoo.com.sg>
2019-07-28 22:41:22 +0800

    some minor edits to tables.sql

--- tables.sql
+++ tables.sql
@@ -10,5 +10,13 @@ CREATE TABLE IF NOT EXISTS users (
 CREATE TABLE IF NOT EXISTS tweets (
     id SERIAL PRIMARY KEY,
     tweets TEXT,
-    user_id INTEGER
+    user_id INTEGER,
+    created_at DATE DEFAULT now()
+);
+
+CREATE TABLE NOT EXISTS pictures (
+		id SERIAL PRIMARY KEY,
+		background_url TEXT,
+		profile_url TEXT,
+		user_id INTEGER
 );

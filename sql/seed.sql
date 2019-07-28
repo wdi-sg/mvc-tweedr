47b214327903d9041f535d50fa4d696b54dfa725
axejacob <email@axeljacob.com>
2019-07-29 00:43:08 +0800

    V1. Tried to implement try-catch syntax.

---
+++ sql/seed.sql
@@ -0,0 +1,7 @@
+INSERT INTO users(username, password, img_url, created_at) VALUES ('tomato', '73892018372991930237898ejbskd0h92ehqeqe','http://pngimg.com/uploads/pokemon/pokemon_PNG154.png', '14/4/2019 01:20:00');
+INSERT INTO users(username, password, img_url, created_at) VALUES ('coffee', 'sdakdjij213qweq9jq3nqk3oiqj3q2om3991231','https://images-na.ssl-images-amazon.com/images/I/51Gji7jFNjL._SX425_.jpg', '14/4/2019 01:20:10');
+
+INSERT INTO tweets(users_username, content, created_at) VALUES ('Donald', 'This is the real fat Trump!', '27/7/2019 03:45:09');
+INSERT INTO tweets(users_username, content, created_at) VALUES ('Peter Parker', 'I love Joes pizza', '27/7/2019 03:47:10');
+INSERT INTO tweets(users_username, content,created_at) VALUES ('RDJ', 'SQuert', '27/7/2019 03:49:34');
+INSERT INTO tweets(users_username, content,created_at) VALUES ('Matt Damon', 'Banana', '27/7/2019 03:55:11');

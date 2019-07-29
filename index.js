a283c4bf3e0df4a7216eca60649ad06e77699d9c
aqilahrajab <YOUR-EMAIL-ADDRESS>
2019-07-29 14:52:31 +0800

    [AQILAH] - REGISTER USER

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ const cookieParser = require('cookie-parser');
  * ===================================
  */
 
+
+
 // Init express app
 const app = express();
 
@@ -59,6 +61,53 @@ setRoutesFunction(app, allModels);
  * Listen to requests on port 3000
  * ===================================
  */
+
+var testRoute = (request, response) => {
+    console.log("test route");
+    response.send("Hey this route works!");
+}; //testruote CT
+
+var registerUser = (request, response) => {
+    console.log("register route setup");
+
+    //process user's input into objects
+    let userInput = request.body;
+    let newData = {};
+    newData.password = sha256(userInput.password);
+    newData.name = userInput.name;
+
+    //Check if there is existing username
+    let existingAcc = "SELECT name FROM users WHERE name=$1";
+    let values = [newData.name];
+
+    pool.query(existingAcc, values, (error,result) => {
+
+        if (error) {
+            console.log("query error", err.stack);
+            response.send("Unable to check database")
+        }// if CT
+
+        else{
+            //since username does not exist check account
+
+        }
+
+    })//pool.query CT
+
+
+
+    response.send("error checking database");
+
+
+}//registerUser CT
+
+
+
+
+
+
+app.get('/test', testRoute);
+
 const PORT = process.env.PORT || 3000;
 
 const server = app.listen(PORT, () => console.log('~~~ Tuning in to the waves of port '+PORT+' ~~~'));

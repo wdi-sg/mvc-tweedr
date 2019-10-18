00ebf33f4a2212d2eac1df172e89825798182397
lhjd <hongjinlau@gmail.com>
2019-10-18 21:15:20 +0800

    Create and seed users and tweets tables

---
+++ seed.sql
@@ -0,0 +1,6 @@
+INSERT INTO users (name) VALUES ('John');
+INSERT INTO users (name) VALUES ('Jane');
+
+INSERT INTO tweets (content, user_id) VALUES ('It''s a great day!', 1);
+INSERT INTO tweets (content, user_id) VALUES ('It''s an awesome day!', 2);
+

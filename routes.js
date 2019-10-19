4da16dde6a2316a30caf08d676f41bea54c9a3ee
dannyozh <danielongzhihao@gmail.com>
2019-10-20 00:45:15 +0800

    created homepage route for tweeder

--- routes.js
+++ routes.js
@@ -1,7 +1,5 @@
 module.exports = (app, allModels) => {
-
-
-  /*
+	/*
    *  =========================================
    *  =========================================
    *  =========================================
@@ -12,9 +10,11 @@ module.exports = (app, allModels) => {
    *  =========================================
    */
 
-  // require the controller
-  const pokemonControllerCallbacks = require('./controllers/pokemon')(allModels);
+	// require the controller
+	const pokemonControllerCallbacks = require('./controllers/pokemon')(allModels);
+	const tweetsControllerCallbacks = require('./controllers/tweets')(allModels);
 
-  app.get('/pokemons', pokemonControllerCallbacks.index);
-  //app.get('/pokemons/:id', pokemons.getPokemon);
+	app.get('/pokemons', pokemonControllerCallbacks.index);
+	//app.get('/pokemons/:id', pokemons.getPokemon);
+	app.get('/', tweetsControllerCallbacks.getAllTweets);
 };

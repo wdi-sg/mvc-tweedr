671be278b4f114eeeb6edc0b1be648d6c4415ad4
lhjd <hongjinlau@gmail.com>
2019-10-19 22:57:10 +0800

    Ask user to add tweet when there is 0 tweet in DB

--- views/tweets/all.jsx
+++ views/tweets/all.jsx
@@ -3,22 +3,30 @@ const Nav = require("../ui/nav");
 
 class All extends React.Component {
   render() {
-      // console.log("***** this.props.allTweets *****\n",this.props.allTweets);
+    // console.log("***** this.props.allTweets *****\n",this.props.allTweets);
+    let tweets = <p>No tweets found, please <a href="/tweets/new">add a new tweet!</a></p>;
+
+    if (this.props.allTweets) {
+      tweets = (
+        <ul>
+          {this.props.allTweets.map(tweet => (
+            <React.Fragment>
+              <li>{tweet.content}</li>
+              <li>@{tweet.name}</li>
+              <hr />
+            </React.Fragment>
+          ))}
+        </ul>
+      );
+    }
+
     return (
       <html>
         <head />
         <body>
           <Nav />
           <h3>All Tweets</h3>
-          <ul>
-              {this.props.allTweets.map(tweet => (
-                  <React.Fragment>
-                      <li>{tweet.content}</li>
-                      <li>@{tweet.name}</li>
-                      <hr/>
-                  </React.Fragment>
-              ))}
-          </ul>
+          {tweets}
         </body>
       </html>
     );

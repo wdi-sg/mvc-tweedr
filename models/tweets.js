179bb3bb14f82141519e3a06e44106ec0143fe44
kevinngth <kevin.ng.th@gmail.com>
2019-10-20 22:23:00 +0800

    Add sort by date to each kind of tweet feed you made.

--- models/tweets.js
+++ models/tweets.js
@@ -2,7 +2,7 @@ module.exports = (dbPoolInstance) => {
 
     let getAll = (req,callback) => {
         let values = [req.cookies.userId];
-        let query = 'SELECT DISTINCT tweets.content, users.username, tweets.users_id, timestamp FROM users INNER JOIN tweets ON (users.id = tweets.users_id) INNER JOIN follows ON (tweets.users_id = follows.users_id) WHERE follows.f_er_users_id =$1 OR tweets.users_id=$1';
+        let query = 'SELECT DISTINCT tweets.content, users.username, tweets.users_id, timestamp FROM users INNER JOIN tweets ON (users.id = tweets.users_id) INNER JOIN follows ON (tweets.users_id = follows.users_id) WHERE follows.f_er_users_id =$1 OR tweets.users_id=$1 ORDER BY timestamp';
         dbPoolInstance.query(query,values,(err,res)=>{
             if (err) {
                 callback(err,null);
@@ -34,7 +34,7 @@ module.exports = (dbPoolInstance) => {
 
     let showFollowersTweets = (req,callback) => {
         let values = [req.cookies.userId];
-        let query = `SELECT DISTINCT tweets.content, users.username, tweets.users_id, timestamp FROM users INNER JOIN tweets ON (users.id = tweets.users_id) INNER JOIN follows ON (tweets.users_id = follows.f_er_users_id) WHERE follows.users_id =$1`;
+        let query = `SELECT DISTINCT tweets.content, users.username, tweets.users_id, timestamp FROM users INNER JOIN tweets ON (users.id = tweets.users_id) INNER JOIN follows ON (tweets.users_id = follows.f_er_users_id) WHERE follows.users_id =$1 ORDER BY timestamp`;
         dbPoolInstance.query(query,values,(err,res)=>{
             if (err) {
                 callback(err,null);

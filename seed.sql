0ff1688bd8185b5a9c2f1fb19586a683698505a3
David M <davidmosk@gmail.com>
2019-10-20 14:05:47 +0800

    split tweedr controller into users

--- seed.sql
+++ seed.sql
@@ -8,12 +8,12 @@ INSERT INTO users
 VALUES
 ('Ivysaur', '644e3e07d58caad7fabbd471256222194263b829114d6acbdd4a4f6513406bc6','ivysaur@gmail.com', 'skinny dipper', 'http://www.serebii.net/pokemongo/pokemon/002.png', '2019-09-25T21:54:23.443', 'active');
 
-INSERT INTO tweet
-	(user_id,tweet,creation_date)
-VALUES
-(1, 'my first tweet - grrr', '2019-09-26T21:54:23.443');
+-- INSERT INTO tweet
+-- 	(user_id,tweet,creation_date)
+-- VALUES
+-- (1, 'my first tweet - grrr', '2019-09-26T21:54:23.443');
 
-INSERT INTO following
-	(user_id_alpha,user_id_beta,creation_date)
-VALUES
-(1, 2, '2019-09-27T21:54:23.443');
+-- INSERT INTO following
+-- 	(user_id_alpha,user_id_beta,creation_date)
+-- VALUES
+-- (1, 2, '2019-09-27T21:54:23.443');
--- tables.sql
+++ tables.sql
@@ -9,16 +9,16 @@ CREATE TABLE IF NOT EXISTS users (
     user_state TEXT
 );
 
-CREATE TABLE IF NOT EXISTS tweet (
-    id SERIAL PRIMARY KEY,
-    user_id INTEGER,
-    tweet TEXT,
-    creation_date DATE
-);
+-- CREATE TABLE IF NOT EXISTS tweet (
+--     id SERIAL PRIMARY KEY,
+--     user_id INTEGER,
+--     tweet TEXT,
+--     creation_date DATE
+-- );
 
-CREATE TABLE IF NOT EXISTS following (
-    id SERIAL PRIMARY KEY,
-    user_id_alpha INTEGER,
-    user_id_beta INTEGER,
-    creation_date DATE
-);
+-- CREATE TABLE IF NOT EXISTS following (
+--     id SERIAL PRIMARY KEY,
+--     user_id_alpha INTEGER,
+--     user_id_beta INTEGER,
+--     creation_date DATE
+-- );

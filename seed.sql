4753d68117b3cd5e28e566f8d00b389e6a22f0ab
Jasmine <jasmine-aips@hotmail.com>
2019-10-20 11:54:36 +0800

    made tables and seed, npm install

---
+++ seed.sql
@@ -0,0 +1,19 @@
+INSERT INTO
+  users (username)
+VALUES
+  ('Calvin');
+INSERT INTO
+  users (username)
+VALUES
+  ('Shroom');
+INSERT INTO
+  tweets (user_id, content)
+VALUES
+  (1, 'Kleinfield makes dresses.');
+INSERT INTO
+  tweets (user_id, content)
+VALUES
+  (
+    2,
+    'Woowowowowowow bananas and mushrooms make a good pancake'
+  );

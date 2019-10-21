8fa986dbe6f902b86fefa9b735ca448735b3c9bf
tansiewling-hotmail <prudence_tan@hotmail.com>
2019-10-21 17:06:24 +0800

    Updated sample data in seed.sql for columns not matching created tables

--- seed.sql
+++ seed.sql
@@ -1,7 +1,7 @@
 -- some users
-INSERT INTO users (username, password) VALUES ('Bear', 'grr123');
-INSERT INTO users (username, password) VALUES ('Meow', 'maumau');
-INSERT INTO users (username, password) VALUES ('Birdo', 'tweet');
+INSERT INTO users (name, email, password) VALUES ('Bear', 'bear@email.com', 'grr123');
+INSERT INTO users (name, email, password) VALUES ('Meow', 'meow@email.com', 'maumau');
+INSERT INTO users (name, email, password) VALUES ('Birdo', 'birdo@email.com', 'tweet');
 
 -- some tweets
 INSERT INTO tweets (post, user_id) VALUES ('So hungry!', 1);

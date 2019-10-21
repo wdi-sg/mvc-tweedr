a4b2df813aa92906aa001ec46bcbd3e46fda96c4
reshinto <terencekong2002@gmail.com>
2019-10-22 00:14:27 +0800

    Convert db settings to .env

--- db.js
+++ db.js
@@ -18,10 +18,10 @@ if ( process.env.DATABASE_URL ) {
   };
 } else {
   configs = {
-    user: "springfield",
-    host: "127.0.0.1",
-    database: "tweedr",
-    port: 5432,
+    user: process.env.user,
+    host: process.env.host,
+    database: process.env.database,
+    port: process.env.port,
   };
 }
 

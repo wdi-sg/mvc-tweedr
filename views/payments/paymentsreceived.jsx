98eec608dc537bccee3fabc523ea247c3d6c76b4
David M <davidmosk@gmail.com>
2019-10-21 21:15:26 +0800

    added payment info into templates

--- views/payments/paymentsreceived.jsx
+++ views/payments/paymentsreceived.jsx
@@ -2,12 +2,27 @@ const React = require("react");
 
 class PaymentsReceived extends React.Component {
   render() {
-    const {type} = this.props;
-    return (
+
+    // console.log(this.props.payments);
+const allPayments = this.props.payments.map( (payment, index) => {
+             
+            return (
+                  <p>Date: {payment.payment_date} <br/>
+                  Transaction ID: {payment.transaction_id}  <br/>
+                  To: {payment.sender_username}  <br/>
+                  From: {payment.recipient_username}  <br/>
+                  Amount {payment.amount} <br/> <hr/> </p>
+
+                )
+
+        })
+        return (
+
+
       <div>
-payments received
+            {allPayments}
       </div>
-    );
+    )
   }
 }
 
--- views/payments/paymentssent.jsx
+++ views/payments/paymentssent.jsx
@@ -2,12 +2,28 @@ const React = require("react");
 
 class PaymentsSent extends React.Component {
   render() {
-    const {type} = this.props;
-    return (
+
+    // console.log(this.props.payments);
+const allPayments = this.props.payments.map( (payment, index) => {
+             
+            return (
+                  <p>Date: {payment.payment_date} <br/>
+                  Transaction ID: {payment.transaction_id}  <br/>
+                  To: {payment.sender_username}  <br/>
+                  From: {payment.recipient_username}  <br/>
+                  Amount {payment.amount} <br/> <hr/> </p>
+                           
+
+                )
+
+        })
+        return (
+
+
       <div>
-payments sent
+            {allPayments}
       </div>
-    );
+    )
   }
 }
 

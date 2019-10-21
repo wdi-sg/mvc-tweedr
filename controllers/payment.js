9bbf3d5586f87202f3e647b3bfd913b2b21eb933
tansiewling-hotmail <prudence_tan@hotmail.com>
2019-10-21 23:57:50 +0800

    Updated to-do notes for when model is merged

--- controllers/payment.js
+++ controllers/payment.js
@@ -14,17 +14,36 @@ module.exports = (db) => {
     // Method to save payment details
     const createPaymentDetails = (request, response) => {
 
-        // Set sample data
-        const samplePaymentData = {
+        // Read payment data
+        const paymentData = {
             senderID: 8, // Login doesn't work, so use hardcoded data first. Will need to get current logged in userID from cookie
             recipientID: request.body.recipientID,
             amount: request.body.amount
         };
 
-        // Call model method to save the payment info HERE
-        response.send(samplePaymentData);
+        /**
+        * ==========================================================================
+        * Call model method to save the payment info HERE
+        *   1. Check method 'createPayment' is created, with INSERT query in model
+        *   2. Check 'payments' key is exported from db.js
+        * ==========================================================================
+        */
 
+        // db.payments.createPayment(paymentData, (error, payments) => {
+
+            // if (error) {
+
+                // console.log("This is from payment controller. There is error inserting the payment record!");
+
+            // } else {
+
+                response.send(paymentData);
+
+            // }
+        // });
     };
+
+
 
     /**
     * ===========================================

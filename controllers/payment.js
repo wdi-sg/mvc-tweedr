b9334b67a4b3eb7d9987f967b6e109f8846be208
tansiewling-hotmail <prudence_tan@hotmail.com>
2019-10-22 00:52:14 +0800

    Added method to get sender's payment

--- controllers/payment.js
+++ controllers/payment.js
@@ -1,10 +1,10 @@
 module.exports = (db) => {
 
     /**
-    * ===========================================
-    * Controller logic
-    * ===========================================
-    */
+     * ===========================================
+     * Controller logic
+     * ===========================================
+     */
 
     // Method to render new payment form
     const getNewPaymentForm = (request, response) => {
@@ -22,36 +22,70 @@ module.exports = (db) => {
         };
 
         /**
-        * ==========================================================================
-        * Call model method to save the payment info HERE
-        *   1. Check method 'createPayment' is created, with INSERT query in model
-        *   2. Check 'payments' key is exported from db.js
-        * ==========================================================================
-        */
+         * ==========================================================================
+         * Call model method to save the payment info HERE
+         *   1. Check method 'createPayment' is created, with INSERT query in model
+         *   2. Check 'payments' key is exported from db.js
+         * ==========================================================================
+         */
 
         // db.payments.createPayment(paymentData, (error, payments) => {
 
-            // if (error) {
+        // if (error) {
 
-                // console.log("This is from payment controller. There is error inserting the payment record!");
+        // console.log("This is from payment controller. There is error inserting the payment record!");
 
-            // } else {
+        // } else {
 
-                response.send(paymentData);
+        response.send(paymentData);
 
-            // }
+        // }
         // });
     };
 
+    /** GET Method to get sender's payment
+     * ==========================================================================
+     * Call model method to get the payment info by sender_id HERE
+     *   1. Check method 'getUserSentPayments' is created, with SELECT query in model
+     *   2. Check 'payments' key is exported from db.js
+     *.  3. Remember to parseInt sender_id in the method in model file
+     * ==========================================================================
+     */
+    const getSenderPayment = (request, response) => {
 
+        let sender_id = parseInt(request.params.sender_id); // This value should be retrieved from current logged in user's user ID cookie.
+        const inputValues = [sender_id];
+        console.log("From Controller: " + inputValues);
+
+        db.payments.getUserSentPayments(inputValues, (error, payments) => {
+
+            if (error) {
+
+                console.log("This is from payment controller. There is error getting the payment record by sender ID!");
+
+            } else {
+
+                if (payments.length > 0) {
+
+                    response.send(payments);
+
+                } else {
+
+                    response.send("You have not made any payment.");
+
+                }
+            }
+        });
+    };
 
     /**
-    * ===========================================
-    * Export controller functions as a module
-    * ===========================================
-    */
+     * ===========================================
+     * Export controller functions as a module
+     * ===========================================
+     */
     return {
         getPaymentForm: getNewPaymentForm,
-        postPayment: createPaymentDetails
+        postPayment: createPaymentDetails,
+        getPaymentBySender: getSenderPayment
     };
 };

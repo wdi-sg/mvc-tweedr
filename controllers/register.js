a390ae1737c34403765d2081b713beff3a18a139
tansiewling-hotmail <prudence_tan@hotmail.com>
2019-10-21 21:10:04 +0800

    Fixed registerUser method

--- controllers/register.js
+++ controllers/register.js
@@ -12,26 +12,41 @@ module.exports = (db) => {
 //     response.render('register')
 
 // });
+
+// const SALT = "nothing";
+
 let registerForm = (request, response) => {
     response.render("users/register");
-  };
+};
 
 
 let registerUser = (request, response) => {
-    console.log(request.body);
-    const newUser = request.body;
+    let inputUsername = request.body.name;
+    let inputEmail = request.body.email;
+    let inputPwd = request.body.password;
+    // let hashedPassword = sha256(inputPwd + SALT);
 
-    const regCallback = (error, registeredUser) => {
+    const newUser = [inputUsername, inputEmail, inputPwd];
+    console.log("In controller: ", newUser);
+
+    db.tweets.registerUser(inputUsername, inputEmail, inputPwd, (error, registeredUser) => {
+        response.send("This is from register controller. User has been created!");
+    });
+};
+
+
+
+   /* const regCallback = (error, registeredUser) => {
 
       let hashedPassword = sha256(newUser.password + SALT);
 
       response.cookie('loggedin', hashedCookie)
             response.cookie('user_id', userID)
-
+            console.log
 
             response.send( queryRes.rows );
 
-            response.render('home');
+            response.render('home');*/
 
              // let userID = queryRes.rows[0].id;
 //         let hashedCookie = sha256(userID + 'loggedin' + SALT);
@@ -50,11 +65,11 @@ let registerUser = (request, response) => {
 //             //response render home, after issue fixed
 //             response.render('home');
 
-    };
+/*    };*/
 
 
 
-  };
+
 
 
 
@@ -68,11 +83,11 @@ let registerUser = (request, response) => {
    * ===========================================
    */
   return {
-    registerForm,
-    registerUser
+    registerForm: registerForm,
+    registerUser: registerUser
   };
 
-}
+ };
 
 
 // //post

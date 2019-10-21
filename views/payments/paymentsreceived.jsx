6bb6e68db215751fe5f808be087c83d2af4444fe
David M <davidmosk@gmail.com>
2019-10-21 20:35:08 +0800

    initial template

---
+++ views/payments/paymentsreceived.jsx
@@ -0,0 +1,14 @@
+const React = require("react");
+
+class PaymentsReceived extends React.Component {
+  render() {
+    const {type} = this.props;
+    return (
+      <div>
+payments received
+      </div>
+    );
+  }
+}
+
+module.exports = PaymentsReceived;
---
+++ views/payments/paymentssent.jsx
@@ -0,0 +1,14 @@
+const React = require("react");
+
+class PaymentsSent extends React.Component {
+  render() {
+    const {type} = this.props;
+    return (
+      <div>
+payments sent
+      </div>
+    );
+  }
+}
+
+module.exports = PaymentsSent;

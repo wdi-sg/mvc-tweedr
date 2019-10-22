b071c7b5b8dfc94aaf8ec3fe2361d495109ae8af
tansiewling-hotmail <prudence_tan@hotmail.com>
2019-10-22 16:32:59 +0800

    Updated SQL query to use $1 instead of variables

--- models/tweedr.js
+++ models/tweedr.js
@@ -7,16 +7,16 @@ module.exports = (dbPoolInstance) => {
 
   // `dbPoolInstance` is accessible within this function scope
 
-  const getUser = (userInput, callback) => {
+  let getUser = (userInput, callback) => {
 
-    const inputValues = [userInput];
+    const inputValues = userInput;
     console.log("In models, inputValues: " + inputValues);
 
     // Why when I use $1 in this query, it can't read the actual input value??
-    let query = "SELECT * FROM users WHERE username = '" + inputValues + "'";
+    let query = "SELECT * FROM users WHERE username = $1";
     console.log("Query statement in models: ", query);
 
-    dbPoolInstance.query(query, (error, queryResult) => {
+    dbPoolInstance.query(query, inputValues, (error, queryResult) => {
 
         if( error ){
 
@@ -27,18 +27,35 @@ module.exports = (dbPoolInstance) => {
 
             // invoke callback function with results after query has executed
 
-            if( queryResult.rows.length > 0 ){
-              callback(null, queryResult.rows);
+            callback(null, queryResult.rows);
 
-            }else{
-              callback(null, queryResult.rows);
-
-            }
         }
     });
+
+    /*let postTweet = (tweetInput, callback) => {
+        const inputValues = [tweetInput];
+        console.log("In models, inputValues: " + inputValues);
+
+        const query = "INSERT INTO tweets (tweetContent, user_id) VALUES ($1, 1) RETURNING *";
+
+        dbPoolInstance.query(query, (error, queryResult) => {
+            if ( error ) {
+
+                // invoke callback function with results after query has executed
+                callback(error, null);
+
+            } else {
+
+                // invoke callback function with results after query has executed
+
+                callback(null, queryResult.rows);
+            }
+        });
+    };*/
   };
 
   return {
     getUser
+    /*postTweet*/
   };
 };

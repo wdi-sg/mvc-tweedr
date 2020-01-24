ec395b59df41f47fcf20d41693ac642d07b6f777
Jess <jessicatanhc@gmail.com>
2020-01-24 20:54:12 +0800

    remove comment

--- models/user.js
+++ models/user.js
@@ -7,7 +7,6 @@ module.exports = (dbPoolInstance) => {
 
   // `dbPoolInstance` is accessible within this function scope
 let validateUser = (username, callback) => {
-    // console.log("HEY FROM MODDELLLLL", whenQueryDoneCallback() );
     let query = 'SELECT * FROM users WHERE username=$1';
     let values = [username];
     dbPoolInstance.query(query, values, (error, queryResult) => {

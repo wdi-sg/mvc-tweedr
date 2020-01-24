0581502a3bde061d5aaad436c725dd72f673f4cd
Jess <jessicatanhc@gmail.com>
2020-01-24 17:14:41 +0800

    Add new route

--- routes.js
+++ routes.js
@@ -14,7 +14,8 @@ module.exports = (app, allModels) => {
 
   // require the controller
   const pokemonControllerCallbacks = require('./controllers/pokemon')(allModels);
-
+  const userControllerCallbacks = require('./controllers/user')(allModels);
   app.get('/pokemons', pokemonControllerCallbacks.index);
+    app.get('/login', userControllerCallbacks.index);
   //app.get('/pokemons/:id', pokemons.getPokemon);
 };

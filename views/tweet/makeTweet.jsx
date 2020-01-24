6388be1399fa391fd69b91bc9732fe1ea55b2f7c
Jess <jessicatanhc@gmail.com>
2020-01-24 21:22:11 +0800

    Add make tweet page

---
+++ views/tweet/makeTweet.jsx
@@ -0,0 +1,26 @@
+var React = require("react");
+var DefaultLayout = require('../layouts/default');
+class LoginPage extends React.Component {
+  render() {
+    return (
+        <DefaultLayout>
+        <div className="container h-100">
+    <div className="row h-100 justify-content-center align-items-center">
+        <div className="col-10 col-md-8 col-lg-6">
+<form className="mt-5" method="POST" action="/newTweet">
+  <div className="form-group">
+    <div className="d-flex flex-row">
+    <input type="text" className="form-control" name="message"/>
+    <button type="submit" className="btn btn-primary btn-customized ml-2">Tweet</button>
+    </div>
+    </div>
+    </form>
+    </div>
+    </div>
+    </div>
+        </DefaultLayout>
+    );
+  }
+}
+
+module.exports =LoginPage;

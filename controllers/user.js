cb93e1cb00b8a39c75eb80da9dd736f9b890b924
Jess <jessicatanhc@gmail.com>
2020-01-24 23:15:21 +0800

    Add cookie and hash

--- controllers/user.js
+++ controllers/user.js
@@ -5,20 +5,34 @@ module.exports = (db) => {
    * Controller logic
    * ===========================================
    */
+  const sha256 = require('js-sha256');
+  const SALT = "saltCookie";
+
+  const isLoggedIn = (request) => {
+     let user_id = request.cookies.user_id;
+     let hashedCookie = sha256(SALT+user_id);
+     return ( request.cookies.loggedIn === hashedCookie) ? true : false;
+  }
 
   let login = (request, response) => {
-        response.render("user/login");
-  };
+    (isLoggedIn(request))
+    ? response.redirect("/tweet") :
+    response.render("user/login");
+  }
 
   let loggedIn = (request, response) => {
- db.users.validateUser(request.body.username,(error, user)=>{
+  db.users.validateUser(request.body.username,(error, user)=>{
     if(error){
       response.send( error );
         }else{
             if(user.length > 1){
                 response.send("Your user name or password is invalid.");
             }else{
-                response.send(user[0]);
+                 let hashedCookie = sha256(SALT+user[0].user_id);
+                response.cookie('user_id', user[0].user_id);
+                response.cookie('username', user[0].username);
+                response.cookie('loggedIn', hashedCookie);
+                response.redirect('/tweet');
             }
         }
     });

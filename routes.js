f0a3c87afac4fbba280a6ce682eff646b310d9fd
Jess <jessicatanhc@gmail.com>
2020-01-24 21:23:30 +0800

    Add tweet route

--- routes.js
+++ routes.js
@@ -13,9 +13,11 @@ module.exports = (app, allModels) => {
    */
 
   // require the controller
-   const pokemonControllerCallbacks = require('./controllers/pokemon')(allModels);
+  const pokemonControllerCallbacks = require('./controllers/pokemon')(allModels);
   const userControllerCallbacks = require('./controllers/user')(allModels);
+  const tweetControllerCallbacks = require('./controllers/tweet')(allModels);
   app.get('/pokemons', pokemonControllerCallbacks.index);
   app.get('/login', userControllerCallbacks.login);
   app.post('/login', userControllerCallbacks.loggedIn);
+  app.get('/tweet', tweetControllerCallbacks.makeTweet);
 };

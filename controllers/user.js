f23a3fda2067d09e87846107d2c32a17744a3f2f
Jess <jessicatanhc@gmail.com>
2020-01-25 16:29:57 +0800

    Add register function

--- controllers/user.js
+++ controllers/user.js
@@ -21,31 +21,52 @@ module.exports = (db) => {
   }
 
   let loggedIn = (request, response) => {
-  db.users.validateUser(request.body.username,(error, user)=>{
+    let username = request.body.username;
+    let password = sha256(request.body.password+SALT);
+  db.users.validateUser(username,password,(error, user)=>{
     if(error){
       response.send( error );
         }else{
-            if(user.length > 1){
+            if(user.length < 1){
                 response.send("Your user name or password is invalid.");
             }else{
                  let hashedCookie = sha256(SALT+user[0].user_id);
                 response.cookie('user_id', user[0].user_id);
                 response.cookie('username', user[0].username);
                 response.cookie('loggedIn', hashedCookie);
-                response.redirect('/tweet');
+                response.redirect('/');
             }
         }
     });
 };
-
+let register = (request,response)=>{
+    response.render("user/register");
+}
+let addUser = (request,response)=>{
+    let username = request.body.username;
+    let password = sha256(request.body.password+SALT)
+    db.users.registerUser(username,password,(error)=>{
+    if(error==="exist"){
+        response.send("username not unique");
+    }
+    else if(error){
+       response.status(500).send("error")
+        }
+        else{
+                response.redirect('/login');
+        }
+    });
+}
   /**
    * ===========================================
    * Export controller functions as a module
    * ===========================================
    */
   return {
-    login: login,
-    loggedIn: loggedIn
+    login,
+    loggedIn,
+    register,
+    addUser
   };
 
 }

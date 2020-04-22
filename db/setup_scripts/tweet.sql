d6ea5f116582dc84ae4bf0cd5ea5daa65e8663ab
Kenan <xinkenan.11@hotmail.com>
2020-04-22 20:23:08 +0800

    add tables

---
+++ db/setup_scripts/tweet.sql
@@ -0,0 +1,102 @@
+insert into tweet (id, title, content, user_id, likes) values (1, 'Object-based discrete approach', 'Sed sagittis. Nam congue, risus semper porta volutpat, quam pede lobortis ligula, sit amet eleifend pede libero quis orci. Nullam molestie nibh in lectus.', 3, 74);
+insert into tweet (id, title, content, user_id, likes) values (2, 'Cross-group modular projection', 'Aenean fermentum. Donec ut mauris eget massa tempor convallis. Nulla neque libero, convallis eget, eleifend luctus, ultricies eu, nibh.', 1, 114);
+insert into tweet (id, title, content, user_id, likes) values (3, 'Balanced even-keeled internet solution', 'Nulla ut erat id mauris vulputate elementum. Nullam varius. Nulla facilisi.
+
+Cras non velit nec nisi vulputate nonummy. Maecenas tincidunt lacus at velit. Vivamus vel nulla eget eros elementum pellentesque.', 1, 170);
+insert into tweet (id, title, content, user_id, likes) values (4, 'Fully-configurable tertiary pricing structure', 'Fusce posuere felis sed lacus. Morbi sem mauris, laoreet ut, rhoncus aliquet, pulvinar sed, nisl. Nunc rhoncus dui vel sem.
+
+Sed sagittis. Nam congue, risus semper porta volutpat, quam pede lobortis ligula, sit amet eleifend pede libero quis orci. Nullam molestie nibh in lectus.', 1, 195);
+insert into tweet (id, title, content, user_id, likes) values (5, 'Front-line tangible groupware', 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Proin risus. Praesent lectus.
+
+Vestibulum quam sapien, varius ut, blandit non, interdum in, ante. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Duis faucibus accumsan odio. Curabitur convallis.', 2, 1);
+insert into tweet (id, title, content, user_id, likes) values (6, 'Front-line bandwidth-monitored analyzer', 'Duis bibendum. Morbi non quam nec dui luctus rutrum. Nulla tellus.', 2, 78);
+insert into tweet (id, title, content, user_id, likes) values (7, 'Enhanced methodical policy', 'Quisque id justo sit amet sapien dignissim vestibulum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nulla dapibus dolor vel est. Donec odio justo, sollicitudin ut, suscipit a, feugiat et, eros.', 2, 100);
+insert into tweet (id, title, content, user_id, likes) values (8, 'Secured 6th generation attitude', 'Duis consequat dui nec nisi volutpat eleifend. Donec ut dolor. Morbi vel lectus in quam fringilla rhoncus.
+
+Mauris enim leo, rhoncus sed, vestibulum sit amet, cursus id, turpis. Integer aliquet, massa id lobortis convallis, tortor risus dapibus augue, vel accumsan tellus nisi eu orci. Mauris lacinia sapien quis libero.', 1, 79);
+insert into tweet (id, title, content, user_id, likes) values (9, 'Total next generation service-desk', 'Quisque porta volutpat erat. Quisque erat eros, viverra eget, congue eget, semper rutrum, nulla. Nunc purus.
+
+Phasellus in felis. Donec semper sapien a libero. Nam dui.', 1, 87);
+insert into tweet (id, title, content, user_id, likes) values (10, 'Operative upward-trending knowledge base', 'Fusce consequat. Nulla nisl. Nunc nisl.
+
+Duis bibendum, felis sed interdum venenatis, turpis enim blandit mi, in porttitor pede justo eu massa. Donec dapibus. Duis at velit eu est congue elementum.', 3, 118);
+insert into tweet (id, title, content, user_id, likes) values (11, 'Distributed mission-critical time-frame', 'Cras non velit nec nisi vulputate nonummy. Maecenas tincidunt lacus at velit. Vivamus vel nulla eget eros elementum pellentesque.', 2, 59);
+insert into tweet (id, title, content, user_id, likes) values (12, 'Extended multi-state focus group', 'Praesent id massa id nisl venenatis lacinia. Aenean sit amet justo. Morbi ut odio.
+
+Cras mi pede, malesuada in, imperdiet et, commodo vulputate, justo. In blandit ultrices enim. Lorem ipsum dolor sit amet, consectetuer adipiscing elit.', 1, 98);
+insert into tweet (id, title, content, user_id, likes) values (13, 'Advanced bandwidth-monitored support', 'Praesent blandit. Nam nulla. Integer pede justo, lacinia eget, tincidunt eget, tempus vel, pede.', 3, 121);
+insert into tweet (id, title, content, user_id, likes) values (14, 'Centralized bifurcated product', 'Aenean fermentum. Donec ut mauris eget massa tempor convallis. Nulla neque libero, convallis eget, eleifend luctus, ultricies eu, nibh.', 2, 2);
+insert into tweet (id, title, content, user_id, likes) values (15, 'Ergonomic foreground neural-net', 'Morbi porttitor lorem id ligula. Suspendisse ornare consequat lectus. In est risus, auctor sed, tristique in, tempus sit amet, sem.
+
+Fusce consequat. Nulla nisl. Nunc nisl.', 1, 38);
+insert into tweet (id, title, content, user_id, likes) values (16, 'Public-key intermediate instruction set', 'Donec diam neque, vestibulum eget, vulputate ut, ultrices vel, augue. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec pharetra, magna vestibulum aliquet ultrices, erat tortor sollicitudin mi, sit amet lobortis sapien sapien non mi. Integer ac neque.', 2, 59);
+insert into tweet (id, title, content, user_id, likes) values (17, 'Assimilated 24 hour architecture', 'Integer tincidunt ante vel ipsum. Praesent blandit lacinia erat. Vestibulum sed magna at nunc commodo placerat.
+
+Praesent blandit. Nam nulla. Integer pede justo, lacinia eget, tincidunt eget, tempus vel, pede.', 2, 195);
+insert into tweet (id, title, content, user_id, likes) values (18, 'Profound system-worthy success', 'Cras non velit nec nisi vulputate nonummy. Maecenas tincidunt lacus at velit. Vivamus vel nulla eget eros elementum pellentesque.', 1, 106);
+insert into tweet (id, title, content, user_id, likes) values (19, 'User-friendly multi-state portal', 'Etiam vel augue. Vestibulum rutrum rutrum neque. Aenean auctor gravida sem.', 2, 195);
+insert into tweet (id, title, content, user_id, likes) values (20, 'Virtual directional alliance', 'Duis bibendum, felis sed interdum venenatis, turpis enim blandit mi, in porttitor pede justo eu massa. Donec dapibus. Duis at velit eu est congue elementum.
+
+In hac habitasse platea dictumst. Morbi vestibulum, velit id pretium iaculis, diam erat fermentum justo, nec condimentum neque sapien placerat ante. Nulla justo.', 3, 193);
+insert into tweet (id, title, content, user_id, likes) values (21, 'Optimized 5th generation productivity', 'In hac habitasse platea dictumst. Morbi vestibulum, velit id pretium iaculis, diam erat fermentum justo, nec condimentum neque sapien placerat ante. Nulla justo.', 3, 146);
+insert into tweet (id, title, content, user_id, likes) values (22, 'Assimilated context-sensitive local area network', 'Praesent id massa id nisl venenatis lacinia. Aenean sit amet justo. Morbi ut odio.
+
+Cras mi pede, malesuada in, imperdiet et, commodo vulputate, justo. In blandit ultrices enim. Lorem ipsum dolor sit amet, consectetuer adipiscing elit.', 2, 91);
+insert into tweet (id, title, content, user_id, likes) values (23, 'Extended cohesive emulation', 'Maecenas leo odio, condimentum id, luctus nec, molestie sed, justo. Pellentesque viverra pede ac diam. Cras pellentesque volutpat dui.
+
+Maecenas tristique, est et tempus semper, est quam pharetra magna, ac consequat metus sapien ut nunc. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Mauris viverra diam vitae quam. Suspendisse potenti.', 3, 106);
+insert into tweet (id, title, content, user_id, likes) values (24, 'Progressive multi-tasking framework', 'Duis consequat dui nec nisi volutpat eleifend. Donec ut dolor. Morbi vel lectus in quam fringilla rhoncus.
+
+Mauris enim leo, rhoncus sed, vestibulum sit amet, cursus id, turpis. Integer aliquet, massa id lobortis convallis, tortor risus dapibus augue, vel accumsan tellus nisi eu orci. Mauris lacinia sapien quis libero.', 2, 6);
+insert into tweet (id, title, content, user_id, likes) values (25, 'Phased client-driven definition', 'Morbi porttitor lorem id ligula. Suspendisse ornare consequat lectus. In est risus, auctor sed, tristique in, tempus sit amet, sem.', 1, 94);
+insert into tweet (id, title, content, user_id, likes) values (26, 'Down-sized explicit paradigm', 'Fusce posuere felis sed lacus. Morbi sem mauris, laoreet ut, rhoncus aliquet, pulvinar sed, nisl. Nunc rhoncus dui vel sem.
+
+Sed sagittis. Nam congue, risus semper porta volutpat, quam pede lobortis ligula, sit amet eleifend pede libero quis orci. Nullam molestie nibh in lectus.', 2, 191);
+insert into tweet (id, title, content, user_id, likes) values (27, 'Team-oriented dedicated protocol', 'Proin interdum mauris non ligula pellentesque ultrices. Phasellus id sapien in sapien iaculis congue. Vivamus metus arcu, adipiscing molestie, hendrerit at, vulputate vitae, nisl.
+
+Aenean lectus. Pellentesque eget nunc. Donec quis orci eget orci vehicula condimentum.', 3, 37);
+insert into tweet (id, title, content, user_id, likes) values (28, 'Multi-lateral tangible encryption', 'Fusce posuere felis sed lacus. Morbi sem mauris, laoreet ut, rhoncus aliquet, pulvinar sed, nisl. Nunc rhoncus dui vel sem.', 3, 52);
+insert into tweet (id, title, content, user_id, likes) values (29, 'Optimized object-oriented extranet', 'Fusce posuere felis sed lacus. Morbi sem mauris, laoreet ut, rhoncus aliquet, pulvinar sed, nisl. Nunc rhoncus dui vel sem.', 1, 10);
+insert into tweet (id, title, content, user_id, likes) values (30, 'Open-architected client-driven middleware', 'Sed sagittis. Nam congue, risus semper porta volutpat, quam pede lobortis ligula, sit amet eleifend pede libero quis orci. Nullam molestie nibh in lectus.', 1, 136);
+insert into tweet (id, title, content, user_id, likes) values (31, 'Multi-tiered 4th generation collaboration', 'Phasellus in felis. Donec semper sapien a libero. Nam dui.', 2, 189);
+insert into tweet (id, title, content, user_id, likes) values (32, 'Customer-focused interactive benchmark', 'Mauris enim leo, rhoncus sed, vestibulum sit amet, cursus id, turpis. Integer aliquet, massa id lobortis convallis, tortor risus dapibus augue, vel accumsan tellus nisi eu orci. Mauris lacinia sapien quis libero.', 2, 160);
+insert into tweet (id, title, content, user_id, likes) values (33, 'Advanced fresh-thinking knowledge user', 'Suspendisse potenti. In eleifend quam a odio. In hac habitasse platea dictumst.
+
+Maecenas ut massa quis augue luctus tincidunt. Nulla mollis molestie lorem. Quisque ut erat.', 1, 121);
+insert into tweet (id, title, content, user_id, likes) values (34, 'Upgradable bandwidth-monitored encryption', 'Cras mi pede, malesuada in, imperdiet et, commodo vulputate, justo. In blandit ultrices enim. Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
+
+Proin interdum mauris non ligula pellentesque ultrices. Phasellus id sapien in sapien iaculis congue. Vivamus metus arcu, adipiscing molestie, hendrerit at, vulputate vitae, nisl.', 1, 169);
+insert into tweet (id, title, content, user_id, likes) values (35, 'Expanded logistical data-warehouse', 'Maecenas leo odio, condimentum id, luctus nec, molestie sed, justo. Pellentesque viverra pede ac diam. Cras pellentesque volutpat dui.', 2, 30);
+insert into tweet (id, title, content, user_id, likes) values (36, 'Stand-alone human-resource time-frame', 'Aliquam quis turpis eget elit sodales scelerisque. Mauris sit amet eros. Suspendisse accumsan tortor quis turpis.
+
+Sed ante. Vivamus tortor. Duis mattis egestas metus.', 1, 169);
+insert into tweet (id, title, content, user_id, likes) values (37, 'Organized neutral frame', 'Cras non velit nec nisi vulputate nonummy. Maecenas tincidunt lacus at velit. Vivamus vel nulla eget eros elementum pellentesque.
+
+Quisque porta volutpat erat. Quisque erat eros, viverra eget, congue eget, semper rutrum, nulla. Nunc purus.', 2, 80);
+insert into tweet (id, title, content, user_id, likes) values (38, 'Adaptive transitional adapter', 'In quis justo. Maecenas rhoncus aliquam lacus. Morbi quis tortor id nulla ultrices aliquet.
+
+Maecenas leo odio, condimentum id, luctus nec, molestie sed, justo. Pellentesque viverra pede ac diam. Cras pellentesque volutpat dui.', 3, 60);
+insert into tweet (id, title, content, user_id, likes) values (39, 'Mandatory 24/7 secured line', 'Maecenas leo odio, condimentum id, luctus nec, molestie sed, justo. Pellentesque viverra pede ac diam. Cras pellentesque volutpat dui.', 2, 98);
+insert into tweet (id, title, content, user_id, likes) values (40, 'Public-key background application', 'Phasellus in felis. Donec semper sapien a libero. Nam dui.
+
+Proin leo odio, porttitor id, consequat in, consequat ut, nulla. Sed accumsan felis. Ut at dolor quis odio consequat varius.', 1, 107);
+insert into tweet (id, title, content, user_id, likes) values (41, 'Open-source zero tolerance forecast', 'Nullam porttitor lacus at turpis. Donec posuere metus vitae ipsum. Aliquam non mauris.', 2, 13);
+insert into tweet (id, title, content, user_id, likes) values (42, 'Sharable zero tolerance hierarchy', 'Mauris enim leo, rhoncus sed, vestibulum sit amet, cursus id, turpis. Integer aliquet, massa id lobortis convallis, tortor risus dapibus augue, vel accumsan tellus nisi eu orci. Mauris lacinia sapien quis libero.
+
+Nullam sit amet turpis elementum ligula vehicula consequat. Morbi a ipsum. Integer a nibh.', 1, 155);
+insert into tweet (id, title, content, user_id, likes) values (43, 'Reverse-engineered asymmetric toolset', 'In hac habitasse platea dictumst. Etiam faucibus cursus urna. Ut tellus.
+
+Nulla ut erat id mauris vulputate elementum. Nullam varius. Nulla facilisi.', 3, 78);
+insert into tweet (id, title, content, user_id, likes) values (44, 'Assimilated modular project', 'Pellentesque at nulla. Suspendisse potenti. Cras in purus eu magna vulputate luctus.', 2, 82);
+insert into tweet (id, title, content, user_id, likes) values (45, 'Enhanced uniform functionalities', 'In congue. Etiam justo. Etiam pretium iaculis justo.', 1, 41);
+insert into tweet (id, title, content, user_id, likes) values (46, 'Ergonomic eco-centric neural-net', 'Praesent id massa id nisl venenatis lacinia. Aenean sit amet justo. Morbi ut odio.', 2, 98);
+insert into tweet (id, title, content, user_id, likes) values (47, 'Distributed motivating capacity', 'Aenean lectus. Pellentesque eget nunc. Donec quis orci eget orci vehicula condimentum.
+
+Curabitur in libero ut massa volutpat convallis. Morbi odio odio, elementum eu, interdum eu, tincidunt in, leo. Maecenas pulvinar lobortis est.', 2, 63);
+insert into tweet (id, title, content, user_id, likes) values (48, 'Cross-group 24 hour approach', 'Phasellus sit amet erat. Nulla tempus. Vivamus in felis eu sapien cursus vestibulum.', 2, 119);
+insert into tweet (id, title, content, user_id, likes) values (49, 'Expanded attitude-oriented portal', 'Phasellus in felis. Donec semper sapien a libero. Nam dui.
+
+Proin leo odio, porttitor id, consequat in, consequat ut, nulla. Sed accumsan felis. Ut at dolor quis odio consequat varius.', 1, 81);
+insert into tweet (id, title, content, user_id, likes) values (50, 'Object-based homogeneous focus group', 'Fusce posuere felis sed lacus. Morbi sem mauris, laoreet ut, rhoncus aliquet, pulvinar sed, nisl. Nunc rhoncus dui vel sem.
+
+Sed sagittis. Nam congue, risus semper porta volutpat, quam pede lobortis ligula, sit amet eleifend pede libero quis orci. Nullam molestie nibh in lectus.', 2, 11);

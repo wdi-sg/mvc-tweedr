e96702a154eb54fa7382ffe31ade4868320c215f
“Marcelchia” <marcel.chiayl@gmail.com>
2020-04-22 22:57:52 +0800

    set up tweet and user

---
+++ models/tweet.js
@@ -0,0 +1,24 @@
+//Export model functions as a module
+
+module.exports = (dbPoolInstance) => {
+
+  // `dbPoolInstance` is accessible within this function scope
+let setTweet = (userId, msg, callback) => {
+    let query = 'INSERT INTO tweets(user_id, message) VALUES($1,$2)';
+    let values = [userId,msg];
+    dbPoolInstance.query(query, values, (error, queryResult) => {
+        console.log(query);
+      callback(error);
+    });
+  };
+  let getTweets = (callback) => {
+    let query = 'SELECT * FROM tweets ORDER BY tweet_id DESC';
+    dbPoolInstance.query(query,(error, queryResult) => {
+      callback(error, queryResult.rows);
+    });
+  };
+  return {
+    setTweet,
+    getTweets
+  };
+};

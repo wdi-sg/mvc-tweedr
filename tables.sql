575945317cb185f0772386b0c583e6598f7f60d2
arindamc14 <arindamchowdhury91@gmail.com>
2020-04-23 09:56:14 +0800

    ari-minor changes

---
+++ tables.sql
@@ -0,0 +1,12 @@
+CREATE TABLE IF NOT EXISTS artists (
+  id SERIAL PRIMARY KEY,
+  userName TEXT,
+  password TEXT
+);
+
+
+CREATE TABLE IF NOT EXISTS artists (
+  id SERIAL PRIMARY KEY,
+  userID TEXT,
+  tweet TEXT
+);

828074da4380fe5846741692bca198fdbabde69e
Denny Koh <denny.koh.kaichao@gmail.com>
2020-04-24 00:07:10 +0800

    Started doing homework

--- db.js
+++ db.js
@@ -10,44 +10,37 @@
  * ===================================================
  */
 
-
-
-const pg = require('pg');
-const url = require('url');
+const pg = require("pg");
+const url = require("url");
 
 var configs;
 
-if( process.env.DATABASE_URL ){
-
+if (process.env.DATABASE_URL) {
   const params = url.parse(process.env.DATABASE_URL);
-  const auth = params.auth.split(':');
+  const auth = params.auth.split(":");
 
   configs = {
     user: auth[0],
     password: auth[1],
     host: params.hostname,
     port: params.port,
-    database: params.pathname.split('/')[1],
-    ssl: true
+    database: params.pathname.split("/")[1],
+    ssl: true,
   };
-
-}else{
+} else {
   configs = {
-    user: 'akira',
-    host: '127.0.0.1',
-    database: 'testdb',
-    port: 5432
+    user: "jasminelee",
+    host: "127.0.0.1",
+    database: "tweeder",
+    port: 5432,
   };
 }
 
-
 const pool = new pg.Pool(configs);
 
-pool.on('error', function (err) {
-  console.log('idle client error', err.message, err.stack);
+pool.on("error", function (err) {
+  console.log("idle client error", err.message, err.stack);
 });
-
-
 
 /*
  * ===================================================
@@ -61,12 +54,9 @@ pool.on('error', function (err) {
  * ===================================================
  */
 
+const allPokemonModelsFunction = require("./models/pokemon");
 
-const allPokemonModelsFunction = require('./models/pokemon');
-
-const pokemonModelsObject = allPokemonModelsFunction( pool );
-
-
+const pokemonModelsObject = allPokemonModelsFunction(pool);
 
 /*
  * ===================================================
@@ -80,7 +70,6 @@ const pokemonModelsObject = allPokemonModelsFunction( pool );
  * ===================================================
  */
 
-
 module.exports = {
   //make queries directly from here
   queryInterface: (text, params, callback) => {
@@ -88,12 +77,12 @@ module.exports = {
   },
 
   // get a reference to end the connection pool at server end
-  pool:pool,
+  pool: pool,
 
   /*
    * ADD APP MODELS HERE
    */
 
   // users: userModelsObject,
-  pokemon: pokemonModelsObject
+  pokemon: pokemonModelsObject,
 };

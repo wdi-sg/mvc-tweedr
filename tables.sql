1acf7a46c8cec2c777a5a2b075360bae67f389ab
YusofGotboudine <yusof95@hotmail.com>
2020-04-23 10:13:21 +0800

    Init Commit

---
+++ tables.sql
@@ -0,0 +1,16 @@
+CREATE TABLE
+IF NOT EXISTS users
+(
+    id SERIAL PRIMARY KEY,
+    name TEXT,
+    password TEXT,
+    email TEXT
+);
+
+CREATE TABLE
+IF NOT EXISTS tweets
+(
+    id SERIAL PRIMARY KEY,
+    content TEXT,
+    user_id INTEGER
+);

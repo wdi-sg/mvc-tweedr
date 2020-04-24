f7bd0fcca3855ffe585871f7a699dbe65b27a84b
kenneththesheep <kenneththesheep@gmail.com>
2020-04-25 01:10:31 +0800

    Standardize look across all pages.

--- views/forms/register.jsx
+++ views/forms/register.jsx
@@ -8,12 +8,28 @@ class register extends React.Component {
       <html>
         <head />
         <link rel={"stylesheet"} href={"https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"} integrity={"sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh"} crossorigin={"anonymous"}></link>
+                        <link rel={"stylesheet"} href={"style/style.css"}></link>
         <body>
 
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+    <div class="navbar-nav">
+      <a class="nav-item nav-link active" href="/">Home <span class="sr-only">(current)</span></a>
+      <a class="nav-item nav-link" href="/login">Login</a>
+      <a class="nav-item nav-link" href="/register">Register</a>
+      <a class="nav-item nav-link" href="/user">Users</a>
+       <a class="nav-item nav-link" href="/tweet">Tweeds</a>
+        <a class="nav-item nav-link" href="/hash">Hash</a>
+        <a class="nav-item nav-link" href="/favorite">Favorites</a>
+    </div>
+  </div>
+</nav>
 
 
-
-          <div class={"container mt-3"}>
+          <div class={"container mt-3 tweetBox"}>
                         <div class={"row align-bottom border"}>
             <div class={"col-12 mt-5 text-center"}>
             <h1  class={"mt-3"}>Register Form</h1>
--- views/user/profile.jsx
+++ views/user/profile.jsx
@@ -38,7 +38,22 @@ class Home extends React.Component {
         <link rel={"stylesheet"} href={"https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"} integrity={"sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh"} crossorigin={"anonymous"}></link>
         <link rel={"stylesheet"} href={"/style/style.css"}></link>
         <body>
-
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+    <div class="navbar-nav">
+      <a class="nav-item nav-link active" href="/">Home <span class="sr-only">(current)</span></a>
+      <a class="nav-item nav-link" href="/login">Login</a>
+      <a class="nav-item nav-link" href="/register">Register</a>
+      <a class="nav-item nav-link" href="/user">Users</a>
+       <a class="nav-item nav-link" href="/tweet">Tweeds</a>
+        <a class="nav-item nav-link" href="/hash">Hash</a>
+        <a class="nav-item nav-link" href="/favorite">Favorites</a>
+    </div>
+  </div>
+</nav>
 
 
 

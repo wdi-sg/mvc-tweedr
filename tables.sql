61bc1c9b924ee81bc2e728a6150491aab4494f88
arindamc14 <arindamchowdhury91@gmail.com>
2020-04-24 09:47:04 +0800

    update

--- tables.sql
+++ tables.sql
@@ -1,12 +1,13 @@
-CREATE TABLE IF NOT EXISTS artists (
+CREATE TABLE IF NOT EXISTS users (
   id SERIAL PRIMARY KEY,
   userName TEXT,
   password TEXT
 );
 
 
-CREATE TABLE IF NOT EXISTS artists (
+CREATE TABLE IF NOT EXISTS tweeds (
   id SERIAL PRIMARY KEY,
-  userID TEXT,
-  tweet TEXT
+  tweed TEXT,
+  hashtag TEXT,
+  user_id TEXT
 );

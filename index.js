80b871a990d42832f6c5730d5dc5298cfd770912
mdazharaw <darkaz62@hotmail.com>
2020-04-27 10:24:15 +0800

    Added script.js

--- index.js
+++ index.js
@@ -23,6 +23,8 @@ app.use(express.urlencoded({
   extended: true
 }));
 
+app.use(express.static('public'))
+
 // Set react-views to be the default view engine
 const reactEngine = require('express-react-views').createEngine();
 
